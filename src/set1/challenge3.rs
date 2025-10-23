83e138922ac423fdea08a9327acc9531883e1eb8
Anura2425 <gmcclurecoleman@gmail.com>
2025-10-23 12:34:22 -0700

    Challenge 3 utils started, finished character scoring function

--- src/set1/challenge3.rs
+++ src/set1/challenge3.rs
@@ -1,16 +1,25 @@
-// use crate::utils::*; COMMENT BACK IN ONCE IN USE
+use crate::utils::*;
 use std::io;
 
 pub fn run(){
-    let mut cipher_text = String::new();
+    let mut hex_string = String::new();
     println!("Input hex string that has been single-byte XOR'd: ");
 
-    io::stdin().read_line(&mut cipher_text).unwrap();
-    let cleaned_cipher_text = cipher_text.trim_end();
+    io::stdin().read_line(&mut hex_string).unwrap();
+    let cleaned_hex_string = hex_string.trim_end();
 
-    println!("{}", cleaned_cipher_text); // placeholder
-    //TODO: write functions for this challenge in utils
+    
+
 }
 
 
 //TODO: write test case for challenge given input
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn passed(){
+
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -2,11 +2,16 @@ use base64::engine::general_purpose;
 use base64::Engine as _;
 use hex::{decode, encode};
 
+static FREQUENT_LETTERS: &str = "ETAOIN SHRDLU";
+
+// SET 1:
+// CHALLENGE 1:
 pub fn hex_to_base64(hex: &str) -> String {
     let bytes: Vec<u8> = hex::decode(hex).expect("invalid hex");
     general_purpose::STANDARD.encode(bytes)
 }
 
+// CHALLENGE 2:
 pub fn fixed_xor(buffer1: &str, buffer2: &str) -> String {
     let bytes1: Vec<u8> = decode(buffer1).unwrap();
     let bytes2: Vec<u8> = decode(buffer2).unwrap();
@@ -18,3 +23,23 @@ pub fn fixed_xor(buffer1: &str, buffer2: &str) -> String {
         .collect();
     encode(xor_bytes)
 }
+
+// CHALLENGE 3:
+pub fn score_character(input: char) -> i32 {
+    let mut score: i32 = 0;
+    if input.is_ascii(){
+        if input.is_alphabetic(){
+            if FREQUENT_LETTERS.contains(input.to_ascii_uppercase()){
+                score += 5;
+            }
+            score += 3;
+        }
+        score += 1;
+    }
+    score
+}
+
+pub fn find_single_byte_xor_key(hex: &str) -> (&str, i32){
+    ("hello", 10)
+}
+
